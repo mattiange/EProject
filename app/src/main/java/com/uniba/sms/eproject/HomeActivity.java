435767e501608576a854df6847e104e93b2f3c0f
Mattia Web Designer <75547604+mattiawebdesigner@users.noreply.github.com>
2022-02-06 21:35:17 +0100

    Effettuate modifiche

--- app/src/main/java/com/uniba/sms/eproject/HomeActivity.java
+++ app/src/main/java/com/uniba/sms/eproject/HomeActivity.java
@@ -1,2 +1,52 @@
-package com.uniba.sms.eproject;public class HomeActivity {
+package com.uniba.sms.eproject;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.navigation.NavigationView;
+import com.google.android.material.snackbar.Snackbar;
+
+public class HomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        //Add Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        ////////////////////////////////////////////////////
+
+        //Floating button
+        FloatingActionButton addFBtn = findViewById(R.id.add);
+        addFBtn.setOnClickListener(view->{
+            Snackbar.make(view, "Da implementare", Snackbar.LENGTH_LONG).show();
+        });
+        ////////////////////////////////////////////////////////////////////////////////
+
+        //Drawer menu
+        DrawerLayout dl = findViewById(R.id.drawer_layout);
+        NavigationView nv = findViewById(R.id.nav_view);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, dl, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        dl.addDrawerListener(toggle);
+        toggle.syncState();
+        ////////////////////////////////////////////////////////////////////////////////
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+
+        return true;
+    }
 }
