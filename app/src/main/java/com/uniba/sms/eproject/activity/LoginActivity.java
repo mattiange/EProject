7b53ead3192c58d886bec55ba40c6cd9ae5ae43f
mangelillo9 <76432732+mangelillo9@users.noreply.github.com>
2022-09-20 17:24:32 +0200

    updates

--- app/src/main/java/com/uniba/sms/eproject/activity/LoginActivity.java
+++ app/src/main/java/com/uniba/sms/eproject/activity/LoginActivity.java
@@ -1,7 +1,10 @@
 package com.uniba.sms.eproject.activity;
 
+import android.accounts.AccountManager;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -67,62 +70,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
-    /**
-     * Entra nell'app tramite login
-     *
-     * @param view
-     */
-    public void loginIntoApp(View view){
-        EditText insertLoginEmail = findViewById(R.id.insertLoginEmail);
-        TextView infoLoginEmail = findViewById(R.id.infoLoginEmail);
-
-        EditText insertLoginPassword = findViewById(R.id.insertLoginPassword);
-        TextView infoLoginPassword = findViewById(R.id.infoLoginPassword);
-
-        //Flag per controllare se i dati inseriti sono validi
-        boolean flag = true;
-
-        /*if ( insertLoginEmail.getText().toString().equals("") ) {
-
-            infoLoginEmail.setVisibility(View.VISIBLE);
-            infoLoginEmail.setText(R.string.info_email);
-
-            Toast.makeText(this, "Login fallito, controlla i campi errati", Toast.LENGTH_SHORT).show();
-            flag = false;
-        } else {
-            infoLoginEmail.setVisibility(View.GONE);
-            flag = true;
-        }
-
-
-
-
-        if ( insertLoginPassword.getText().toString().equals("") ) {
-
-            infoLoginPassword.setVisibility(View.VISIBLE);
-            infoLoginPassword.setText(R.string.null_password);
-
-            Toast.makeText(this, "Login fallito, controlla i campi errati", Toast.LENGTH_SHORT).show();
-
-            flag = false;
-        } else {
-            infoLoginPassword.setVisibility(View.GONE);
-            flag = true;
-        }
-
-
-        if(flag){
-            DbManager db = new DbManager(this);
-            db.login(insertLoginEmail.getText().toString(), insertLoginPassword.getText().toString());
-
-            startActivity(new Intent(this, HomeActivity.class));
-        }else{
-            Snackbar.make((ConstraintLayout)findViewById(R.id.loginLayout), "Dati di login errati", Snackbar.LENGTH_LONG).show();
-        }*/
-    }
-
     public void forgotPassword(View view){
-
         Toast.makeText(this, "Da implementare", Toast.LENGTH_SHORT).show();
 
     }
