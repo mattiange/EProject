b7b2f56eec71629d4d16b63a0b8101482cfcb9b8
mangelillo9 <mattia.angelillo@gmail.com>
2022-10-12 14:40:39 +0200

    bug fixed
    fissato bug per selezione tmk o commerciale

--- app/src/main/java/it/sms/eproject/activity/MainActivity.java
+++ app/src/main/java/it/sms/eproject/activity/MainActivity.java
@@ -1,25 +1,21 @@
 package it.sms.eproject.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.drawerlayout.widget.DrawerLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.ui.AppBarConfiguration;
+import androidx.navigation.ui.NavigationUI;
 
-import android.os.Build;
 import android.os.Bundle;
-import android.widget.Toast;
+import android.view.Menu;
 
-import java.time.LocalDate;
-import java.util.ArrayList;
+import com.google.android.material.navigation.NavigationView;
 
 import it.sms.eproject.R;
-import it.sms.eproject.activity.CallbackFragment;
-import it.sms.eproject.activity.LoginFragment;
-import it.sms.eproject.activity.RegisterFragment;
-import it.sms.eproject.data.classes.Permesso;
-import it.sms.eproject.data.classes.Utente;
-import it.sms.eproject.database.DbManager;
-import it.sms.eproject.util.Util;
 
 public class MainActivity extends AppCompatActivity implements CallbackFragment {
 
@@ -27,12 +23,25 @@ public class MainActivity extends AppCompatActivity implements CallbackFragment 
     FragmentManager fragmentManager;
     FragmentTransaction fragmentTransaction;
 
+    private AppBarConfiguration mAppBarConfiguration;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_main);
 
-        addFragment();
+        DrawerLayout dl = findViewById(R.id.drawer_layout);
+        NavigationView nv = findViewById(R.id.nav_view);
+        mAppBarConfiguration = new AppBarConfiguration.Builder(R.id.nav_home, R.id.nav_gallery, R.id.nav_share)
+                                                        .setOpenableLayout(dl)
+                                                                .build();
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
+        NavigationUI.setupWithNavController(nv, navController);
+
+
+        //addFragment();
     }
 
     /**
@@ -62,5 +71,20 @@ public class MainActivity extends AppCompatActivity implements CallbackFragment 
     @Override
     public void changeFragment() {
         replaceFragment();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_main);
+        return NavigationUI.navigateUp(navController, mAppBarConfiguration)
+                || super.onSupportNavigateUp();
     }
 }
