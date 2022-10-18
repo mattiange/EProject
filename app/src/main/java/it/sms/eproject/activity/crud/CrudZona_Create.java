b4891f4ee830872f1ba214adbdbf54bde7dc2be6
giandobc <g.bucci15@studenti.uniba.it>
2022-10-18 16:57:40 +0200

    Creazione  - Crud per la cancellazione del Museo,  - Crud per la cancellazione dell' Oggetto,  - Crud per la creazione della Zona  - Crud per la visualizzazione della lista dei Musei

---
+++ app/src/main/java/it/sms/eproject/activity/crud/CrudZona_Create.java
@@ -0,0 +1,22 @@
+package it.sms.eproject.activity.crud;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import it.sms.eproject.R;
+
+public class CrudZona_Create extends Fragment {
+
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.crudzona_create_fragment, container, false);
+        return v;
+    }
+
+
+}
