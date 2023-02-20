7e82088fddf96bf2c57e5d96492cbfd66a6b954c
mangelillo9 <mattia.angelillo@gmail.com>
2023-02-20 22:07:48 +0100

    Modificata creazione del percorso

--- app/src/main/java/it/sms/eproject/fragment/home/crud/percorso/CrudPercorso_Create.java
+++ app/src/main/java/it/sms/eproject/fragment/home/crud/percorso/CrudPercorso_Create.java
@@ -3,6 +3,7 @@ package it.sms.eproject.fragment.home.crud.percorso;
 import static it.sms.eproject.util.EseguiFragment.changeFragment;
 
 import android.annotation.SuppressLint;
+import android.app.AlertDialog;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -20,6 +21,7 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -30,6 +32,8 @@ import androidx.constraintlayout.widget.ConstraintLayout;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -44,6 +48,7 @@ import it.sms.eproject.database.DBMuseo;
 import it.sms.eproject.database.DBOggetto;
 import it.sms.eproject.database.DBPercorso;
 import it.sms.eproject.fragment.home.crud.liste.ListaPercorso;
+import it.sms.eproject.util.EseguiFragment;
 import it.sms.eproject.util.OnSwipeTouchListener;
 
 public class CrudPercorso_Create extends Fragment {
@@ -97,6 +102,8 @@ public class CrudPercorso_Create extends Fragment {
 
     Bundle bundle;
 
+    ScrollView sv;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -112,16 +119,26 @@ public class CrudPercorso_Create extends Fragment {
      * Inizializzazione dei campi
      */
     private void init(){
+        this.sv                 = v.findViewById(R.id.sv);
         durataPercorso          = v.findViewById(R.id.etDurataPercorso);
         nomePercorso            = v.findViewById(R.id.etNomePercorso);
         descrizionePercorso     = v.findViewById(R.id.etDescrizionePercorso);
         errorNomePercorso       = v.findViewById(R.id.errorNomePercorso);
         //errorMuseiPercorso      = v.findViewById(R.id.errorMuseiPercorso);
 
+
+
         this.bundle = new Bundle();
         this.codice_citta = Long.parseLong(getArguments().getString("codice_citta"));
 
         durataPercorso.setText("0");
+
+        FloatingActionButton btnSalva = v.findViewById(R.id.btn_salva_percorso);
+
+        //Salvataggio del percorso
+        btnSalva.setOnClickListener((view)->{
+            salva();
+        });
 
 
         TextView titolo = v.findViewById(R.id.titolo);
@@ -172,47 +189,129 @@ public class CrudPercorso_Create extends Fragment {
 
             cl.addView(ll);
 
-            ll.setOnTouchListener(new OnSwipeTouchListener(getContext(), posizione){
-                @Override
-                public void onSwipeLeft() {
-                    List<Museo> musei = new DBMuseo(getContext()).elencoMuseiByCitta(codice_citta);
 
-                    Toast.makeText(getContext(), "Left", Toast.LENGTH_SHORT).show();
-                    museiScelti.remove(musei.get(this.posizione));
+            int finalPosizione = posizione;
+            ll.setOnLongClickListener(v1 -> {
 
-                    EditText durataVisistaEt = v.findViewById(R.id.etDurataPercorso);
+                boolean trovato = false;
 
-                    int totale_durata_visita = Integer.parseInt(durataVisistaEt.getText().toString());
-                    totale_durata_visita -= musei.get(this.posizione).getDurata_visita();
-                    durataVisistaEt.setText(String.valueOf(totale_durata_visita));
-
-                    ll.setBackground(getResources().getDrawable(R.drawable.rounded_corners));
-                }
-                @Override
-                public void onSwipeRight() {
-                    List<Museo> musei = new DBMuseo(getContext()).elencoMuseiByCitta(codice_citta);
-
-                    Toast.makeText(getContext(), "Right", Toast.LENGTH_SHORT).show();
-                    museiScelti.add(musei.get(this.posizione));
-
-                    EditText durataVisistaEt = v.findViewById(R.id.etDurataPercorso);
-
-                    int totale_durata_visita = Integer.parseInt(durataVisistaEt.getText().toString());
-                    totale_durata_visita += musei.get(this.posizione).getDurata_visita();
-                    durataVisistaEt.setText(String.valueOf(totale_durata_visita));
-
-                    ll.setBackground(getResources().getDrawable(R.drawable.rounded_corners_green));
+                for(Museo ms : this.museiScelti){
+                    if(ms.getID() == m.getID()){
+                        trovato = true;
+                    }
                 }
 
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    return gestureDetector.onTouchEvent(event);
+                if(!trovato) {
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Aggiungi voce")
+                            .setMessage("Vuoi aggiunger questa voce al percorso?")
+                            .setPositiveButton("Si", (dialog, which) -> {
+                                museiScelti.add(m);
+
+                                EditText durataVisistaEt = v.findViewById(R.id.etDurataPercorso);
+
+                                int totale_durata_visita = Integer.parseInt(durataVisistaEt.getText().toString());
+                                totale_durata_visita += musei.get(finalPosizione).getDurata_visita();
+                                durataVisistaEt.setText(String.valueOf(totale_durata_visita));
+
+                                ll.setBackground(getResources().getDrawable(R.drawable.rounded_corners_green));
+                            })
+                            .setNegativeButton("No", (dialog, which) -> {
+                            })
+                            .show();
+                }else{
+                    AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+                    builder.setTitle("Elimina voce")
+                            .setMessage("Vuoi eliminare questa voce dal percorso?")
+                            .setPositiveButton("Si", (dialog, which) -> {
+                                museiScelti.remove(m);
+
+                                EditText durataVisistaEt = v.findViewById(R.id.etDurataPercorso);
+
+                                int totale_durata_visita = Integer.parseInt(durataVisistaEt.getText().toString());
+                                totale_durata_visita -= musei.get(finalPosizione).getDurata_visita();
+                                durataVisistaEt.setText(String.valueOf(totale_durata_visita));
+
+                                ll.setBackground(getResources().getDrawable(R.drawable.rounded_corners));
+                            })
+                            .setNegativeButton("No", (dialog, which) -> {
+                            })
+                            .show();
                 }
+
+                return true;
             });
 
         }
     }
 
+    /**
+     * Controlla se tutti i campi obbligatori sono correttamente compilati
+     */
+    private boolean controllaCampiObbligatori(){
+        boolean controlloNome, controlloMusei;
+
+        if(nomePercorso.getText().toString().trim().isEmpty()){
+            //errorNomePercorso.setVisibility(View.VISIBLE);
+            Toast.makeText(getContext(), getResources().getString(R.string.msg_error_percorso_nome_obbligatorio), Toast.LENGTH_SHORT).show();
+
+            controlloNome = false;
+        }else{
+            //errorNomePercorso.setVisibility(View.INVISIBLE);
+
+            controlloNome = true;
+        }
+;
+        if(museiScelti.size() > 0 || oggettiScelti.size() > 0){
+            //errorMuseiPercorso.setVisibility(View.INVISIBLE);
+            controlloMusei = true;
+        }else{
+            //errorMuseiPercorso.setVisibility(View.VISIBLE);
+            Toast.makeText(getContext(), getResources().getString(R.string.msg_error_percorso_musei), Toast.LENGTH_SHORT).show();
+
+            controlloMusei = false;
+        }
+
+        return controlloNome && controlloMusei;
+    }
+
+    /**
+     * Salva il percorso nel database
+     */
+    private void salva(){
+        if(controllaCampiObbligatori()){
+
+            //Lettura dei campi
+            String np = nomePercorso.getText().toString();
+            String ddp = durataPercorso.getText().toString();
+            String dp = descrizionePercorso.getText().toString();
+            SharedPreferences pref = getContext().getSharedPreferences("credenziali", 0);
+
+
+            long codice = new DBPercorso(getContext())
+                    .inserisciPercorso(
+                            new Percorso(
+                                    np,
+                                    dp,
+                                    Integer.parseInt(ddp),
+                                    Integer.parseInt(pref.getString("user_id", "-1")),
+                                    Long.parseLong(getArguments().getString("codice_citta"))
+                            ),
+                            museiScelti,
+                            oggettiScelti
+                    );
+            this.bundle.putLong("codice_percorso", codice);
+            changeFragment(()->{
+                Fragment fragment = new CRUDVisualizzaPercorso();
+                fragment.setArguments(this.bundle);
+
+                FragmentManager fragmentManager = getActivity().getSupportFragmentManager();
+                FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                fragmentTransaction.replace(R.id.fragmentContainer, fragment);
+                fragmentTransaction.addToBackStack(null).commit();
+            });
+        }
+    }
 
 }
 
